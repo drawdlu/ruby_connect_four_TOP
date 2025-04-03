fb3c43db3607361862e1c6374479d5034a00f09c
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-03 21:51:48 +0800

    feat: Place a piece on empty column

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,20 @@
+# frozen_string_literal: true
 
+# Board for colored pieces
+class Board
+  attr_reader :board_indices, :board
+
+  def initialize
+    @board = Array.new(6) { Array.new(7, nil) }
+    @board_indices = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6 }
+  end
+
+  def place_piece_on_board(color, letter)
+    index_one = @board_indices[letter]
+    index_two = 5
+
+    index_two -= 1 until @board[index_one][index_two].nil?
+
+    @board[index_one][index_two] = color
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,13 +3,15 @@ require_relative '../lib/board'
 describe Board do
   describe '#place_piece_on_board' do
     subject(:connect_board) { described_class.new }
-    context 'when a piece is placed on an empty board' do
+    context 'when a piece is placed on an empty column' do
       it 'will go to the bottom' do
+        color = :red
+        letter = :A
         expected_result = Array.new(6) { Array.new(7, nil) }
-        expected_result[0][5] = :red
+        expected_result[0][5] = color
 
         connect_board.place_piece_on_board(color, letter)
-        board_result = connect_board.get_instance_variable(:@board)
+        board_result = connect_board.instance_variable_get(:@board)
         expect(board_result).to eq(expected_result)
       end
     end
