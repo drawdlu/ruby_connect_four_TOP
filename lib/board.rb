e36aeac82bf17b93302b7741cb9d364c8ff90e82
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-03 22:06:17 +0800

    test: Piece stacks on top of other pieces on board

--- lib/board.rb
+++ lib/board.rb
@@ -6,12 +6,12 @@ class Board
 
   def initialize
     @board = Array.new(6) { Array.new(7, nil) }
-    @board_indices = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6 }
+    @board_indices = { A: 0, B: 1, C: 2, D: 3, E: 4, F: 5, G: 6 }.freeze
   end
 
   def place_piece_on_board(color, letter)
     index_one = @board_indices[letter]
-    index_two = 5
+    index_two = 5 # last index of a row
 
     index_two -= 1 until @board[index_one][index_two].nil?
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,12 +3,26 @@ require_relative '../lib/board'
 describe Board do
   describe '#place_piece_on_board' do
     subject(:connect_board) { described_class.new }
+    let(:color) { :red }
+    let(:letter) { :A }
+    let(:expected_result) { Array.new(6) { Array.new(7, nil) } }
+
     context 'when a piece is placed on an empty column' do
       it 'will go to the bottom' do
-        color = :red
-        letter = :A
-        expected_result = Array.new(6) { Array.new(7, nil) }
         expected_result[0][5] = color
+
+        connect_board.place_piece_on_board(color, letter)
+        board_result = connect_board.instance_variable_get(:@board)
+        expect(board_result).to eq(expected_result)
+      end
+    end
+
+    context 'when a piece is placed on a column with 1 piece' do
+      it 'will place the above the first piece' do
+        expected_result[0][5] = color
+        one_piece_board = Marshal.load(Marshal.dump(expected_result))
+        expected_result[0][4] = color
+        connect_board.instance_variable_set(:@board, one_piece_board)
 
         connect_board.place_piece_on_board(color, letter)
         board_result = connect_board.instance_variable_get(:@board)
