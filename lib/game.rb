f8206e8d71b6bf928e12e9b1909cb7f67ad35edd
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-04 21:31:04 +0800

    feat: Accept a valid move

---
+++ lib/game.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative 'board'
+require_relative 'players'
+
+class Game
+  MAX_MOVES = 42
+
+  def initialize
+    @board = Board.new
+    @players = Players.new
+    @active_player = @players.player1
+  end
+
+  def get_move
+    loop do
+      move = nil
+
+      until !move.nil? && @board.board_indices.key?(move.upcase.to_sym)
+        print 'Enter your move: '
+        move = gets.chomp
+      end
+
+      move_symbol = move.upcase.to_sym
+
+      return move_symbol if @board.space_available?(move_symbol)
+    end
+  end
+end
