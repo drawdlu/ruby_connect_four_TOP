2b501a26768a7dbbedf19baa8716c68bd61288c0
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-04-04 20:29:38 +0800

    feat: Get player names on initialization of Players

--- lib/players.rb
+++ lib/players.rb
@@ -3,6 +3,16 @@
 # for player data
 
 class Players
+  attr_reader :player1, :player2
+
+  def initialize
+    player_name1 = get_name(1)
+    player_name2 = get_name(2)
+
+    @player1 = { player_name1 => :red }
+    @player2 = { player_name2 => :blue }
+  end
+
   def get_name(player_num)
     length = 0
     while length < 1
